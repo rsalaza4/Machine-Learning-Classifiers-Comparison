40adb1da428eca3cfaa767826ffa659aed42e7e9
Roberto Salazar <56412788+rsalaza4@users.noreply.github.com>
2020-05-31 10:55:15 -0400

    Update Code.py

--- Code.py
+++ Code.py
@@ -7,3 +7,135 @@ import seaborn as sns
 
 # Set random seed
 np.random.seed(42)
+
+# Load csv file
+df = pd.read_csv('framingham.csv')
+
+# View top 5 rows
+df.head()
+
+# EXPLORATORY DATA ANALYSIS
+
+# Visualize male/female ratio
+sns.countplot(x=df["male"]).set_title("Male/Female Ratio")
+
+# Visualize the classes distributions
+sns.countplot(x=df["TenYearCHD"]).set_title("Class Distribution")
+
+# Visualize the classes distributions by gender
+sns.countplot(x="TenYearCHD", hue="male", data=df).set_title('Class Distribution by Gender')
+
+# DATA CLEANING
+
+# Check if there are any null values
+df.isnull().values.any()
+
+# Remove null values
+df = df.dropna()
+
+# Check if there are any null values
+df.isnull().values.any()
+
+# DATA SET BALANCING
+
+# Specify features columns
+X = df.drop(columns="TenYearCHD", axis=0)
+
+# Specify target column
+y = df["TenYearCHD"]
+
+# Import required library for resampling
+from imblearn.under_sampling import RandomUnderSampler
+
+# Instantiate Random Under Sampler
+rus = RandomUnderSampler(random_state=42)
+
+# Perform random under sampling
+df_data, df_target = rus.fit_resample(X, y)
+
+# Visualize new classes distributions
+sns.countplot(df_target).set_title('Balanced Data Set')
+
+# MDOELS BUILDING AND PERFOMANCE EVALUATION
+
+# Import required libraries for performance measures
+from sklearn.metrics import make_scorer
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import precision_score
+from sklearn.metrics import recall_score
+from sklearn.metrics import f1_score
+from sklearn.model_selection import cross_validate
+
+# Define dictionary with performance measures
+scoring = {'accuracy':make_scorer(accuracy_score), 
+           'precision':make_scorer(precision_score),
+           'recall':make_scorer(recall_score), 
+           'f1_score':make_scorer(f1_score)}
+
+# Import required libraries for machine learning classifiers
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import LinearSVC
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.naive_bayes import GaussianNB
+
+# Instantiate the machine learning classifiers
+log_model = LogisticRegression(max_iter=10000)
+svc_model = LinearSVC(dual=False)
+dtr_model = DecisionTreeClassifier()
+rfc_model = RandomForestClassifier()
+gnb_model = GaussianNB()
+
+# Define the models evaluation function
+def models_evaluation(X, y, folds):
+    
+    '''
+    X : data set features
+    y : data set target
+    folds : number of cross-validation folds
+    
+    '''
+    
+    # Perform cross-validation to each machine learning classifier
+    log = cross_validate(log_model, X, y, cv=folds, scoring=scoring)
+    svc = cross_validate(svc_model, X, y, cv=folds, scoring=scoring)
+    dtr = cross_validate(dtr_model, X, y, cv=folds, scoring=scoring)
+    rfc = cross_validate(rfc_model, X, y, cv=folds, scoring=scoring)
+    gnb = cross_validate(gnb_model, X, y, cv=folds, scoring=scoring)
+
+    # Create a data frame with the models perfoamnce measures scores
+    models_scores_table = pd.DataFrame({'Logistic Regression':[log['test_accuracy'].mean(),
+                                                               log['test_precision'].mean(),
+                                                               log['test_recall'].mean(),
+                                                               log['test_f1_score'].mean()],
+                                       
+                                     'Support Vector Classifier':[svc['test_accuracy'].mean(),
+                                                                  svc['test_precision'].mean(),
+                                                                  svc['test_recall'].mean(),
+                                                                  svc['test_f1_score'].mean()],
+                                       
+                                      'Decision Tree':[dtr['test_accuracy'].mean(),
+                                                       dtr['test_precision'].mean(),
+                                                       dtr['test_recall'].mean(),
+                                                       dtr['test_f1_score'].mean()],
+                                       
+                                      'Random Forest':[rfc['test_accuracy'].mean(),
+                                                       rfc['test_precision'].mean(),
+                                                       rfc['test_recall'].mean(),
+                                                       rfc['test_f1_score'].mean()],
+                                       
+                                      'Gaussian Naive Bayes':[gnb['test_accuracy'].mean(),
+                                                              gnb['test_precision'].mean(),
+                                                              gnb['test_recall'].mean(),
+                                                              gnb['test_f1_score'].mean()]},
+                                      
+                                      index=['Accuracy', 'Precision', 'Recall', 'F1 Score'])
+    
+    # Add 'Best Score' column
+    models_scores_table['Best Score'] = models_scores_table.idxmax(axis=1)
+    
+    # Return models performance measures scores data frame
+    return(models_scores_table)
+  
+# Run models_evaluation function
+models_evaluation(df_data, df_target, 5)
