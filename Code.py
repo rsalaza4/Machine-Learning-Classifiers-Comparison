7b14cc4a1d8091793765f34a7c8283e66cdf5f54
Roberto Salazar <56412788+rsalaza4@users.noreply.github.com>
2020-05-31 10:43:22 -0400

    Create Code.py

---
+++ Code.py
@@ -0,0 +1,9 @@
+# Import required libraries
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+%matplotlib inline
+
+# Set random seed
+np.random.seed(42)
